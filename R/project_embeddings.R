dbf5b86c54214c5e05246ed586d11e521f47798c
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2024-11-21 10:32:37 +0100

    Move column selection step to a different function

--- R/project_embeddings.R
+++ R/project_embeddings.R
@@ -1,7 +1,9 @@
 #' @export
 project_embeddings <- function(embeddings) {
 
-  projed_embs_pca <- prcomp(embeddings)
+  projed_embs_pca <- do.call(rbind, embeddings$embedding) |>
+    as.data.frame() |>
+    prcomp()
 
   embs_2D <- projed_embs_pca$x |>
     tibble::as_tibble(rownames = "pkg") |>
--- R/summarize_embeddings.R
+++ R/summarize_embeddings.R
@@ -4,10 +4,6 @@ summarize_embeddings <- function(embeddings) {
   embeddings |>
     dplyr::mutate(weights = dplyr::if_else(stringr::str_detect(content, "^\\-\\-\\-"), 10, 1)) |>
     dplyr::select(-content, -file_name) |>
-    dplyr::summarize(avg_embedding = list(matrixStats::colWeightedMedians(do.call(rbind, embedding), w = weights)), .by = package_name) |>
-    dplyr::pull(avg_embedding, name = package_name) |>
-    dplyr::bind_rows() |>
-    t() |>
-    as.data.frame()
+    dplyr::summarize(embedding = list(matrixStats::colWeightedMedians(do.call(rbind, embedding), w = weights)), .by = package_name)
 
 }
